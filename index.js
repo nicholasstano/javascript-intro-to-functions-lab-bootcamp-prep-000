eebc329ab5ece0c0d2fcaafefc5614d59f59d462
Nicholas Stano <nicholasstano@gmail.com>
2019-04-28 18:09:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 function shout(string) {
   return string.toUpperCase()
 }
+//returns STRING 
 
 function whisper(string) {
-  return string.toLowerCase() 
+  return string.toLowerCase()
 }
+//returns string
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(shout(string))
 }
+//console.log message in all caps
+//BEST WAY TO WRITE THIS?  OR console.log(string.toUpperCase()) better?  Obviously this is the only way to do it if I didn't write the code above.
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(whisper(string))
 }
+//console.log message in all lower case
 
 function sayHiToGrandma(string) {
-  if (whisper(string) === string) {
-    return "I can't hear you!"
+  if (string === whisper(string)) {
+    return `I can't hear you!`
   }
-  else if (shout(string) === string) {
-    return "YES INDEED!"
+  else if (string === shout(string)) {
+    return `YES INDEED!`
   }
-  
-  else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+  else if (string === `I love you, Grandma.`) {
+    return `I love you, too.`
   }
 }
+//if the string is in all lower case Grandma cannot hear it.
+//if the string is all upper case Grandma says YES INDEED. 
+//if the string matches i love you grandma then she responds.
+
 
